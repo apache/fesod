68d238b3d540ffd4ab87c5ff9929741b8e8620d9
hoojo_@126.com <hoojo_@126.com>
2025-09-30 18:01:53 +0800

    test: add new test current row data object during writing #598

---
+++ fesod-examples/src/test/java/org/apache/fesod/excel/demo/write/RecordCellWriteHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.excel.demo.write;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.fesod.excel.util.BooleanUtils;
+import org.apache.fesod.excel.write.handler.CellWriteHandler;
+import org.apache.fesod.excel.write.handler.context.CellWriteHandlerContext;
+import org.apache.poi.ss.usermodel.Cell;
+
+/**
+ * 拦截器中单元格上下文可以获取到行数据
+ */
+@Slf4j
+public class RecordCellWriteHandler implements CellWriteHandler {
+
+    @Override
+    public void afterCellDispose(CellWriteHandlerContext context) {
+        Cell cell = context.getCell();
+
+        if (BooleanUtils.isFalse(context.getHead()) && cell.getColumnIndex() != 0) {
+            RecordData record = (RecordData) context.getOriginalRecord();
+            log.info("写入数据：{}", record);
+        }
+    }
+}

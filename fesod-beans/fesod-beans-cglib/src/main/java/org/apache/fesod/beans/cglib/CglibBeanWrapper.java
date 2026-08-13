2bb2978eab422eca1da073ef0a55c7b88425a9f3
Bengbengbalabalabeng <baicaixiaozhan1512@163.com>
2026-08-13 15:37:02 +0800

    feat: add fesod-beans/fesod-beans-cglib module

---
+++ fesod-beans/fesod-beans-cglib/src/main/java/org/apache/fesod/beans/cglib/CglibBeanWrapper.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.fesod.beans.cglib;
+
+import java.util.Map;
+import java.util.Set;
+import org.apache.fesod.common.beans.BeanWrapper;
+import org.apache.fesod.common.util.ValidateUtils;
+import org.apache.fesod.shaded.cglib.beans.BeanMap;
+import org.apache.fesod.shaded.cglib.core.DefaultNamingPolicy;
+
+public final class CglibBeanWrapper implements BeanWrapper {
+
+    private final BeanMap delegate;
+
+    public CglibBeanWrapper(Object bean) {
+        ValidateUtils.notNull(bean, "The bean instance must not be null");
+
+        this.delegate = initBeanMap(bean);
+    }
+
+    private BeanMap initBeanMap(Object bean) {
+        BeanMap.Generator gen = new BeanMap.Generator();
+        gen.setBean(bean);
+        gen.setContextClass(bean.getClass());
+        gen.setNamingPolicy(FesodSheetNamingPolicy.INSTANCE);
+        return gen.create();
+    }
+
+    @Override
+    public Object getProperty(String propertyName) {
+        return delegate.get(propertyName);
+    }
+
+    @Override
+    public void setProperty(String propertyName, Object value) {
+        delegate.put(propertyName, value);
+    }
+
+    @Override
+    public void setProperties(Map<String, Object> properties) {
+        delegate.putAll(properties);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Set<String> getPropertyNames() {
+        return delegate.keySet();
+    }
+
+    @Override
+    public boolean containsProperty(String propertyName) {
+        return delegate.containsKey(propertyName);
+    }
+
+    @Override
+    public int getPropertySize() {
+        return delegate.size();
+    }
+
+    @Override
+    public Class<?> getPropertyType(String propertyName) {
+        return delegate.getPropertyType(propertyName);
+    }
+
+    @Override
+    public Object unwrap() {
+        return delegate.getBean();
+    }
+
+    public static class FesodSheetNamingPolicy extends DefaultNamingPolicy {
+        public static final FesodSheetNamingPolicy INSTANCE = new FesodSheetNamingPolicy();
+
+        @Override
+        protected String getTag() {
+            return "ByFesodCGLIB";
+        }
+    }
+}

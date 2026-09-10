89056be1a1a853e2dec4b889b51130e90ecedfe8
SummerC0zyR0ck <adsdas08@163.com>
2026-09-10 07:31:29 +0000

    feat: add java.sql.Time converter support

---
+++ fesod-sheet/src/test/java/org/apache/fesod/sheet/converter/SqlTimeRoundTripTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.apache.fesod.sheet.converter;
+
+import java.io.File;
+import java.sql.Time;
+import java.util.Collections;
+import java.util.List;
+import org.apache.fesod.sheet.testkit.Tags;
+import org.apache.fesod.sheet.testkit.base.AbstractExcelTest;
+import org.apache.fesod.sheet.testkit.enums.ExcelFormat;
+import org.apache.fesod.sheet.testkit.helpers.RoundTripHelper;
+import org.apache.fesod.sheet.testkit.params.ExcelFormatSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+
+@Tag(Tags.ROUND_TRIP)
+class SqlTimeRoundTripTest extends AbstractExcelTest {
+    @ParameterizedTest
+    @ExcelFormatSource
+    void roundTrip(ExcelFormat format) throws Exception {
+        File file = createTempFile(format);
+        SqlTimeRoundTripData input = new SqlTimeRoundTripData();
+        input.setTime(Time.valueOf("12:34:56"));
+        List<SqlTimeRoundTripData> result =
+                RoundTripHelper.writeAndRead(file, SqlTimeRoundTripData.class, Collections.singletonList(input));
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(input.getTime(), result.get(0).getTime());
+    }
+}
